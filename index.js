bce67a3ce6f8c5ac98fa2d6c996cf70c8852abf8
Sammy-CK <sammycherono2696@gmail.com>
2022-12-06 14:29:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData (userName, userEmail){
+
+  let newUser = {
+    name : userName,
+    email : userEmail
+  }
+
+ return fetch('http://localhost:3000/users',
+  {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
+      Accept : "application/json"
+    },
+    body : JSON.stringify(newUser)
+  })
+  .then(resp => resp.json())
+  .then(data => {
+    let body = document.querySelector('body')
+    body.append(data.id)
+    console.log(data.id)
+  })
+  .catch(error => {
+    let body = document.querySelector('body')
+    body.append(error.message);
+  })
+
+
+}
+
+
+submitData("Sammy", "star@gmail.com")
+submitData("Sammy", "star@gmail.com")
